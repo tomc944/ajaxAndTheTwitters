331d639cff5425768019413bb32a78fa8a0d495f
App Academy Student <student@appacademy.io>
2015-12-04 17:34:52 -0800

    ajax follow toggle !!!

--- AjaxTwitter/frontend/follow_toggle.js
+++ AjaxTwitter/frontend/follow_toggle.js
@@ -1,13 +1,16 @@
 var FollowToggle = function (el) {
   this.$el = $(el);
   this.userId = parseInt(this.$el.data('user-id'));
-  this.followState =
-      this.setFollowState(this.$el.data('initial-follow-state'));
-  // this.setFollowState("true");
+  this.followState = this.$el.data('initial-follow-state');
   this.render();
+  this.bindEvents();
 };
 
 FollowToggle.prototype = {
+  bindEvents: function () {
+    this.$el.on("click", this.handleClick.bind(this));
+  },
+
   render: function () {
     if (this.followState === "followed") {
       this.$el.text("Follow!");
@@ -15,14 +18,39 @@ FollowToggle.prototype = {
       this.$el.text("Unfollow!");
     }
   },
-  setFollowState: function (booleanState) {
-    if (booleanState === true) {
-      return "followed";
-    } else if (booleanState === false) {
-      return "unfollowed";
+
+  handleClick: function (e) {
+    e.preventDefault();
+    var request = {
+      url: "/users/" + this.userId + "/follow",
+      dataType: "json",
+      data: { user_id: this.userId },
+      success: function () {
+        console.log('hey');
+        this.switchState();
+        this.render();
+      }.bind(this),
+      error: function () {
+        console.log("Failfest!");
+      }
+    };
+
+    if (this.followState === "followed") {
+      $.extend(request, {method: "DELETE"});
+    } else if (this.followState === "unfollowed") {
+      $.extend(request, {method: "POST"});
+    }
+
+    $.ajax(request);
+  },
+
+  switchState: function () {
+    if (this.followState === "followed") {
+      this.followState = "unfollowed";
+    } else {
+      this.followState = "followed";
     }
   }
-
 };
 
 module.exports = FollowToggle;
